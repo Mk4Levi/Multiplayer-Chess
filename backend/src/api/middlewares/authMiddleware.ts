e94c082fb7dae0f8c0da4596bd078d92515ce8f6
Manthan <mksoul1811@gmail.com>
2024-08-17 00:08:49 +0530

    create Middlewares

---
+++ backend/src/api/middlewares/authMiddleware.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from 'express';
+
+export const isAuthenticated = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    res.status(401).json({ message: 'You are not authenticated!' });
+  }
+};
